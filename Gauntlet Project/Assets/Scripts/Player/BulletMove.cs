c967576fc369a4e072893a9f6564ce8e9b112405
adamzer0 <noobertrulz@gmail.com>
2021-05-14 14:30:30 -0700

    fixed the last few deltatimes

--- Gauntlet Project/Assets/Scripts/Player/BulletMove.cs
+++ Gauntlet Project/Assets/Scripts/Player/BulletMove.cs
@@ -7,16 +7,16 @@ public class BulletMove : MonoBehaviour
     //speed
     public float speed = 0.5f;
     //how long the projectile exists for.
-    public int lifespan = 30;
+    public float lifespan = 30;
     public int damage = 1;
     
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * speed);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * 60);
         //lifespan goes down by 1 every frame and at 0 causes the projectile to dissapate;
-        lifespan -= 1;
+        lifespan -= 1 * Time.deltaTime * 60;
         if (lifespan <= 0)
         {
             Destroy(gameObject);
--- Gauntlet Project/Assets/Scripts/Player/RemoveSelf.cs
+++ Gauntlet Project/Assets/Scripts/Player/RemoveSelf.cs
@@ -7,16 +7,18 @@ public class RemoveSelf : MonoBehaviour
     //this script simply makes something remove itself
     //after 2 ticks, giving everything a chance
     //to die to it.
-    public int removedelay = 3;
+    public float removedelay = 3;
+    public int removedelaynodelta = 3;
     public GameObject myowner;
     //for bombs
     public int mypower = 1;
     void Update()
     {
-        removedelay -= 1;
+        removedelay -= 1 * Time.deltaTime * 60;
+        removedelaynodelta -= 1;
        
        
-        if (removedelay <= 0)
+        if (removedelay <= 0 && removedelaynodelta <= 0)
         {
             Destroy(gameObject);
         }
