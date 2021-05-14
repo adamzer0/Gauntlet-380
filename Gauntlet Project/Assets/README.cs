2149ddb9e2e1346475c7fb36ec94523386dfb6a1
adamzer0 <noobertrulz@gmail.com>
2021-05-13 20:02:02 -0700

    finished level 2 started level 3 added readme

---
+++ Gauntlet Project/Assets/README.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class README : MonoBehaviour
+{
+    /* The game is pretty simple, avoid enemies, attack them and win
+     * To start press 1 (Warrior), 2 (Valk) 3 (Wizard) or 4 (Elf) 
+     * More players are also spawned by pressing these buttons
+     * If a class has died it cannot be respawned.
+     * If all spawned player are killed its game over!
+     * Get to the goal to get to the next level (Green square)
+     * 
+     * food is for healing (looks like a drumstick)... Dont shoot it.
+     * Gold is for score (Golden sphere)
+     * Bombs deal damage to everything on screen. If you shoot one, it activates automatically
+     * at a weaker level.
+     * Keys look like small yellow pills.
+     * 
+     * Doors are white squares, open them with a key.
+     * 
+     * Controls:
+     * 
+     * Warrior
+     * Valk
+     * Wizard
+     * Elf
+     * 
+     * 
+     * To start the game, start at level 0. No other level will work.
+     * (well gameover will work, but thats a sad way to start!)   
+     * 
+     */
+}
